61aadf62c68b79c570af02545716a2c4d8a12155
mwai <mwmtingwi5@gmail.com>
2015-08-28 15:23:49 +0200

    modified:   scripts/defaults.php

--- scripts/defaults.php
+++ scripts/defaults.php
@@ -9,17 +9,9 @@ ini_set('post_max_size', '60M');
 ini_set('upload_max_filesize', '60M');
 ini_set('max_execution_time', 600);
 ini_set('max_input_time', -1);
-//CONSTANT VARIABLES WEBSITE
-//define('DOMAIN','www.bicomalawi.org');
 
-//CONSTANT LOCAL MACHINE
+//CONSTANT SERVER MACHINE
 define('DOMAIN','http://localhost/standar_car/');
-
-//DB VARIABLES WEBSITE
-//define('DB_USER','bico_super');
-//define('DB_PASSWORD','Jesusislord2665');
-//define('DB_HOST','localhost');
-//define('DB_NAME','bico_web');
 
 //DB LOCAL MACHINE
 define('DB_USER','root');
